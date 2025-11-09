184599c743a8a6ea91332c620f7d0e8667077b1c
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-11-09 01:22:57 -0800

    1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 
 const Statistics = ({ good, neutral, bad }) => {
+  if (good + neutral + bad === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>statistics</h1>
