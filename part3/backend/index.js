3aefa963134708ecf4015eb01f9064246318c613
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-11-12 14:10:08 -0800

    3.8*: Phonebook backend step 8

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 let phoneBook = [
@@ -25,6 +26,10 @@ let phoneBook = [
 ];
 
 app.use(express.json());
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/api/persons", (request, response) => response.json(phoneBook));
 
