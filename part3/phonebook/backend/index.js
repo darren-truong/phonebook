4e472b2f8417b67db26f784e22ff3de02d0c34f6
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-11-17 14:12:03 -0800

    3.13: Phonebook database, step 1

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,30 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const Person = require("./models/person");
 
 const app = express();
-
-let phoneBook = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.use(express.static("dist"));
 app.use(express.json());
@@ -33,7 +12,11 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-app.get("/api/persons", (request, response) => response.json(phoneBook));
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
+});
 
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
@@ -101,7 +84,7 @@ app.get("/info", (request, response) => {
   response.send(html);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
