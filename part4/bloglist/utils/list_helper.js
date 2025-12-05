e17c7689d5a7f446f594af2c2001e79604017e5a
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-12-05 02:36:49 -0800

    4.4: Helper Functions and Unit Tests, step 2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) =>
+  blogs.reduce((totalLikes, blog) => totalLikes + blog.likes, 0);
+
 module.exports = {
   dummy,
+  totalLikes,
 };
